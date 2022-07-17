96d344b84403f3f8b4cab94ba99ec5f542c2cd46
BlueShdw <no2.nemesis@gmail.com>
2022-07-17 14:04:39 +0430

    pointing system

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ class Dot:
         self.i = i
         self.j = j
 
-    def connect_dot(self, neighbour, c):
+    def connectDot(self, neighbour, c):
         self.neighbours.append([neighbour, c])
+
+
+class Line:
+    def __init__(self, p1, p2, c):
+        self.p1 = p1
+        self.p2 = p2
+        self.color = c
 
 
 class Player:
@@ -15,30 +22,72 @@ class Player:
     def __init__(self, c):
         self.color = c
 
-    def add_point(self):
+    def addPoint(self):
         self.points += 1
 
 
 class Ground:
     dots = []
     lines = set()
+    dotsGround = []
 
     def __init__(self, m, k):
         self.m = m
         self.k = k
 
         for i in range(m):
+            self.dots.append(k * [])
             for j in range(k):
-                self.dots.append(Dot(i, j))
+                self.dots[i][j] = Dot(i, j)
 
     def drawLine(self, m1, m2, k1, k2, c):
-        for dot in self.dots:
-            if dot.i == m1 and dot.j == k1:
-                for neighbour in self.dots:
-                    if neighbour.i == m2 and neighbour.j == k2:
-                        dot.connect_dot(neighbour, c)
-                        neighbour.connect_dot(dot, c)
-                        self.lines.add({dot, neighbour, c})
+        dot = self.dots[m1][k1]
+        neighbour = self.dots[m2][k2]
+
+        if (not(Line(dot, neighbour, c) in self.lines) and not(Line(neighbour, dot, c) in self.lines)):
+            dot.connectDot(neighbour, c)
+            neighbour.connectDot(dot, c)
+            self.lines.add(Line(dot, neighbour, c))
+
+
+def checkGame(ground, dots, lines, lastLine):
+    lastColor = lastLine.color
+
+    if (Line(dots[lastLine.p1.i][lastLine.p1.j+1], dots[lastLine.p2.i][lastLine.p2.j+1]) in lines
+                or
+                Line(dots[lastLine.p2.i][lastLine.p2.j+1],
+                     dots[lastLine.p1.i][lastLine.p1.j+1]) in lines
+            ):
+        ground.drawLine(lastLine.p1.i, lastLine.p1.j+1,
+                        lastLine.p2.i, lastLine.p2.j+1, lastColor)
+        players[lastColor].addPoint()
+
+    if (Line(dots[lastLine.p1.i][lastLine.p1.j-1], dots[lastLine.p2.i][lastLine.p2.j-1]) in lines
+                or
+                Line(dots[lastLine.p2.i][lastLine.p2.j-1],
+                     dots[lastLine.p1.i][lastLine.p1.j-1]) in lines
+            ):
+        ground.drawLine(lastLine.p1.i, lastLine.p1.j-1,
+                        lastLine.p2.i, lastLine.p2.j-1, lastColor)
+        players[lastColor].addPoint()
+
+    if (Line(dots[lastLine.p1.i+1][lastLine.p1.j], dots[lastLine.p2.i+1][lastLine.p2.j]) in lines
+                or
+                Line(dots[lastLine.p2.i+1][lastLine.p2.j],
+                     dots[lastLine.p1.i+1][lastLine.p1.j]) in lines
+            ):
+        ground.drawLine(lastLine.p1.i+1, lastLine.p1.j,
+                        lastLine.p2.i+1, lastLine.p2.j, lastColor)
+        players[lastColor].addPoint()
+
+    if (Line(dots[lastLine.p1.i-1][lastLine.p1.j], dots[lastLine.p2.i-1][lastLine.p2.j]) in lines
+                or
+                Line(dots[lastLine.p2.i-1][lastLine.p2.j],
+                     dots[lastLine.p1.i-1][lastLine.p1.j]) in lines
+            ):
+        ground.drawLine(lastLine.p1.i-1, lastLine.p1.j,
+                        lastLine.p2.i-1, lastLine.p2.j, lastColor)
+        players[lastColor].addPoint()
 
 
 n = int(input('number of playes: '))
@@ -49,3 +98,4 @@ ground = Ground(int(m), int(k))
 players = []
 for i in range(n):
     players.append(Player(i+1))
+
