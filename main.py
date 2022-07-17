266cde5c696602a31ddd7f717f3f8bf2efe84fe5
BlueShdw <no2.nemesis@gmail.com>
2022-07-17 13:23:40 +0430

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+class Player:
+    def __init__(self, _name, _squares):
+        self.name = _name
+        self.squares = _squares
+
+    def number_of_squares(self):
+        print(self.squares);
+
+class Playground:
+    def __init__(self, _row, _column):
+        self.row = _row
+        self.column = _column
+
+    def show_play_ground(self):
+        for i in range():
+            print(i);
+
+
+
+p1 = Player('Bob', 10)
+p1.number_of_squares()  # Prints: Hello, my name is Bob and I am 25 years old!
+
+
